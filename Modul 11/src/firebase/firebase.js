097493570fe8bfc05ecfd9a75867f78256b94469
ivancrisnanda <48828444+ivancrisnanda@users.noreply.github.com>
2020-06-14 22:15:11 +0700

    Laporan Praktikum Modul 11

---
+++ Modul 11/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBR0JZ4yLAAoDw1PvM-BMKblX9MxjIyS1g",
+    authDomain: "minggu11-4eace.firebaseapp.com",
+    databaseURL: "https://minggu11-4eace.firebaseio.com",
+    projectId: "minggu11-4eace",
+    storageBucket: "minggu11-4eace.appspot.com",
+    messagingSenderId: "485037033252",
+    appId: "1:485037033252:web:538edfd8c7e28fd3e7d5b1",
+    measurementId: "G-8DYZBDKX1D"
+  };
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
