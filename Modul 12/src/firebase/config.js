db2099e7a0103ab9804b1c7c372b1569ac924feb
ivancrisnanda <48828444+ivancrisnanda@users.noreply.github.com>
2020-06-14 22:29:52 +0700

    Praktikum Modul 12

---
+++ Modul 12/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBR0JZ4yLAAoDw1PvM-BMKblX9MxjIyS1g",
+    authDomain: "minggu11-4eace.firebaseapp.com",
+    databaseURL: "https://minggu11-4eace.firebaseio.com",
+    projectId: "minggu11-4eace",
+    storageBucket: "minggu11-4eace.appspot.com",
+    messagingSenderId: "485037033252",
+    appId: "1:485037033252:web:538edfd8c7e28fd3e7d5b1",
+    measurementId: "G-8DYZBDKX1D"
+  };
+
+  export default firebaseConfig;
